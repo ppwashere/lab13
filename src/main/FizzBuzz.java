7f88e08fdced858557bdfa9ace939834d698cdb1
Sopida Thippayawat <sopida.t@kkumail.com>
2022-10-11 17:45:45 +0700

    update

---
+++ src/main/FizzBuzz.java
@@ -0,0 +1,9 @@
+package main;
+
+public class FizzBuzz {
+
+	public FizzBuzz() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/test/mainTest.java
+++ src/test/mainTest.java
@@ -1,14 +1,31 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import main.FizzBuzz;
 
 class mainTest {
 
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("TC1: normal number")
+	void testNormalNumber() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String expected = "1";
+		String actaul = fizzbuzz.checkBuzzFizz(input);
+		assertEquals(expected,actaul);
+	}
+	@Test
+	@DisplayName("TC2: buzzfizz number")
+	void testBuzzFizzNumber() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		String expected = "3";
+		String actaul = fizzbuzz.checkBuzzFizz(input);
+		assertEquals(expected,actaul);
 	}
 
 }
