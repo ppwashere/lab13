512dfedf638d87a23f559ee8581e1dde8d6db473
areeya <areeya.la@kkumail.com>
2022-10-11 21:23:04 +0700

    update 4

--- src/main/FizzBuzz.java
+++ src/main/FizzBuzz.java
@@ -6,11 +6,15 @@ public class FizzBuzz {
 		if(input%3==0) {
 			System.out.println("Fizz");
 			return "Fizz";
+			
+		}else if(input%5==0){
+			System.out.println("Buzz");
+			return "Buzz";
+			
 		}else {
 			System.out.println(input);
 			return String.valueOf(input);
 		}
-		
 		
 	}
 	
